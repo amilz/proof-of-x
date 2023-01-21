e731f8209615ff618affc264e5368e39d5d06696
amilz.sol <85324096+amilz@users.noreply.github.com>
2023-01-21 10:18:05 -0800

    use pub rpc

---
+++ utils/constants.ts
@@ -0,0 +1,6 @@
+import { clusterApiUrl } from "@solana/web3.js";
+
+export const NETWORK = clusterApiUrl('mainnet-beta'); // TO DO REF ENV
+export const TOKEN_MINT = 'DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263'//TO DO REF ENV
+export const MIN_BURN_AMT = 1;
+export const NUM_DECIMALS = 5;
