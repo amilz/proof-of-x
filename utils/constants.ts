ec3cae608b97b0415ef79865fcc89113d141a7fc
amilz.sol <85324096+amilz@users.noreply.github.com>
2023-01-21 15:17:57 -0800

    fallback to BONK if no ENV

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,6 +1,12 @@
 import { clusterApiUrl } from "@solana/web3.js";
 
-export const NETWORK = clusterApiUrl('mainnet-beta'); // TO DO REF ENV
-export const TOKEN_MINT = 'DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263'//TO DO REF ENV
-export const MIN_BURN_AMT = 1;
-export const NUM_DECIMALS = 5;
+export const NETWORK = process.env.NEXT_PUBLIC_RPC
+    ?? clusterApiUrl('mainnet-beta');
+export const TOKEN_MINT = process.env.NEXT_PUBLIC_TOKEN_MINT
+    ?? 'DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263';
+export const MIN_BURN_AMT = Number(process.env.NEXT_PUBLIC_MIN_BURN_AMT)
+    ?? 1000000;
+export const NUM_DECIMALS = Number(process.env.NEXT_PUBLIC_NUM_DECIMALS)
+    ?? 5;
+export const TOKEN_NAME = process.env.NEXT_PUBLIC_TOKEN_NAME
+    ?? 'BONK';
