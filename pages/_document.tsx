e5fc5696610141d10a87219163e5971b7c9bc2a8
amilz.sol <85324096+amilz@users.noreply.github.com>
2023-01-21 13:07:58 -0800

    Add social metadata

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,25 +4,29 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head >
-      <link rel="icon" href="/favicon.ico" />
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon-16x16.png"
-          />
-          <link rel="manifest" href="/site.webmanifest" />
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta property="og:title" content="Proof of X" />
+        <meta property="og:type" content="image/png" />
+        <meta property="og:url" content="https://github.com/amilz/proof-of-x" />
+        <meta property="og:image" content="/burn.png" />
       </Head>
       <body>
         <Main />
